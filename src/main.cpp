988618161227c0398b64256f697d0c465ee3863c
AsafHagany21 <asafhag@gmail.com>
2024-05-18 20:32:55 +0300

    IMU class

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,72 @@
-#include <Arduino.h>
+#include "imu/IMUGY85.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+IMUGY85 imu;
+double ax, ay, az, gx, gy, gz, mx, my, mz, roll, pitch, yaw;
+int lastTime = 0;
+void setup()
+{
+  Serial.begin(9600);
+  imu.init();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void printAccel()
+{
+  imu.getAcceleration(&ax, &ay, &az);
+  Serial.print("Accel:");Serial.print("\t");
+  Serial.print(ax);Serial.print("\t");
+  Serial.print(ay);Serial.print("\t");
+  Serial.print(az);Serial.print("\t");
+  Serial.println();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void printGyro()
+{
+  imu.getGyro(&gx, &gy, &gz);
+  Serial.print("Gyro:");Serial.print("\t");
+  Serial.print(gx);Serial.print("\t");
+  Serial.print(gy);Serial.print("\t");
+  Serial.print(gz);Serial.print("\t");
+  Serial.println();
+}
+
+void printMag()
+{
+  imu.getMag(&mx, &my, &mz);
+  Serial.print("Mag:");Serial.print("\t");
+  Serial.print(mx);Serial.print("\t");
+  Serial.print(my);Serial.print("\t");
+  Serial.print(mz);Serial.print("\t");
+  Serial.println();
+}
+void printRollPitchYaw()
+{
+  roll = imu.getRoll();
+  pitch = imu.getPitch();
+  yaw = imu.getYaw();
+  Serial.print(pitch);Serial.print("\t");
+  Serial.print(roll);Serial.print("\t");
+  Serial.print(yaw);Serial.print("\t");
+  Serial.println();
+}
+
+void loop()
+{
+  imu.update();
+  if(millis()-lastTime > 1000){
+    Serial.println();
+    roll = imu.getRoll();
+    pitch = imu.getPitch();
+    yaw = imu.getYaw();
+    Serial.print(pitch);Serial.print("\t");
+    Serial.print(roll);Serial.print("\t");
+    Serial.print(yaw);Serial.print("\t");
+    Serial.println();
+    printAccel();
+  printGyro();
+  printMag();
+  // printRollPitchYaw();
+    lastTime = millis();
+  }
+  
+  delay(10);
 }
