ad60371a1c037b26b30d602efc7e308f00857b41
Yonatan Tzury <ytzury@gmail.com>
2024-05-18 14:53:51 +0300

    Add ifdef for each sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
 
+#define LoraTest 1
+#define GPSTest 0
+#define IMUTest 0
+
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  #ifdef LoraTest
+  #endif
+
+  #ifdef GPSTest
+  #endif
+
+  #ifdef IMUTest
+  #endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  #ifdef LoraTest
+  #endif
+
+  #ifdef GPSTest
+  #endif
+
+  #ifdef IMUTest
+  #endif
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
