4f0a2c4a3c1f8cc01323a25cdeee7745147a9686
Omri Kornblau <omri.354@gmail.com>
2024-06-15 17:43:06 +0300

    BNO055 works

--- src/imu/imu.cpp
+++ src/imu/imu.cpp
@@ -1,88 +1,183 @@
+#include <Arduino.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
 #include "imu.h"
 
-#include "../imu_gy85/IMUGY85.h"
+#include <Wire.h>
 
-IMUGY85 imu;
+
+double orientationData, angVelocityData, linearAccelData, magnetometerData, accelerometerData, gravityData;
 double ax, ay, az, gx, gy, gz, mx, my, mz, roll, pitch, yaw;
 int lastTime = 0;
 
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29, &Wire);
+
 void setupIMU() {
-    imu.init();
-    Serial.println("Init imu succssessfully");
+    Wire.begin();
+
+    if (!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while (1);
+    }
+
+    Serial.println("Success");
 }
 
-void printAccel() {
-    imu.getAcceleration(&ax, &ay, &az);
-    Serial.print("Accel:");
-    Serial.print("\t");
-    Serial.print(ax);
-    Serial.print("\t");
-    Serial.print(ay);
-    Serial.print("\t");
-    Serial.print(az);
-    Serial.print("\t");
-    Serial.println();
-}
-
-void printGyro() {
-    imu.getGyro(&gx, &gy, &gz);
-    Serial.print("Gyro:");
-    Serial.print("\t");
-    Serial.print(gx);
-    Serial.print("\t");
-    Serial.print(gy);
-    Serial.print("\t");
-    Serial.print(gz);
-    Serial.print("\t");
-    Serial.println();
-}
-
-void printMag() {
-    imu.getMag(&mx, &my, &mz);
-    Serial.print("Mag:");
-    Serial.print("\t");
-    Serial.print(mx);
-    Serial.print("\t");
-    Serial.print(my);
-    Serial.print("\t");
-    Serial.print(mz);
-    Serial.print("\t");
-    Serial.println();
-}
-void printRollPitchYaw() {
-    roll = imu.getRoll();
-    pitch = imu.getPitch();
-    yaw = imu.getYaw();
-    Serial.print(pitch);
-    Serial.print("\t");
-    Serial.print(roll);
-    Serial.print("\t");
-    Serial.print(yaw);
-    Serial.print("\t");
-    Serial.println();
-}
 
 void loopIMU() {
-    imu.update();
+    displayData();
+}
 
-    if (millis() - lastTime > 1000) {
-        Serial.println();
-        roll = imu.getRoll();
-        pitch = imu.getPitch();
-        yaw = imu.getYaw();
-        Serial.print(pitch);
-        Serial.print("\t");
-        Serial.print(roll);
-        Serial.print("\t");
-        Serial.print(yaw);
-        Serial.print("\t (");
-        Serial.print(imu.getHeading());
-        Serial.print(") \t");
-        Serial.println();
-        printAccel();
-        printGyro();
-        printMag();
-        // printRollPitchYaw();
-        lastTime = millis();
+void displayData() {
+    sensors_event_t orientationData;
+    // , angVelocityData, linearAccelData, magnetometerData, accelerometerData, gravityData;
+    bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+    // bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+    // bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+    // bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+    // bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    // bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+
+    printEvent(&orientationData);
+    // printEvent(&angVelocityData);
+    // printEvent(&linearAccelData);
+    // printEvent(&magnetometerData);
+    // printEvent(&accelerometerData);
+    // printEvent(&gravityData);
+
+    // int8_t boardTemp = bno.getTemp();
+    // Serial.println();
+    // Serial.print(F("temperature: "));
+    // Serial.println(boardTemp);
+
+    uint8_t system, gyro, accel, mag = 0;
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    if (!(gyro == 3 && mag == 3 && accel == 3)) {
+        Serial.print("Calibration: Sys=");
+        Serial.print(system);
+        Serial.print(" Gyro=");
+        Serial.print(gyro);
+        Serial.print(" Accel=");
+        Serial.print(accel);
+        Serial.print(" Mag=");
+        Serial.println(mag);
     }
+
+    delay(200);
+}
+
+void displaySensorDetails(void)
+{
+    sensor_t sensor;
+    bno.getSensor(&sensor);
+    Serial.println("------------------------------------");
+    Serial.print("Sensor:       "); Serial.println(sensor.name);
+    Serial.print("Driver Ver:   "); Serial.println(sensor.version);
+    Serial.print("Unique ID:    "); Serial.println(sensor.sensor_id);
+    Serial.print("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+    Serial.print("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+    Serial.print("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+    Serial.println("------------------------------------");
+    Serial.println("");
+    delay(500);
+}
+
+void checkConnection() {
+    byte error, address;
+    int nDevices;
+
+    Serial.println("Scanning...");
+
+    nDevices = 0;
+    for (address = 1; address < 127; address++)
+    {
+        // The i2c_scanner uses the return value of
+        // the Write.endTransmisstion to see if
+        // a device did acknowledge to the address.
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+
+        if (error == 0)
+        {
+            Serial.print("I2C device found at address 0x");
+            if (address < 16)
+                Serial.print("0");
+            Serial.print(address, HEX);
+            Serial.println("  !");
+
+            nDevices++;
+        }
+        else if (error == 4)
+        {
+            Serial.print("Unknown error at address 0x");
+            if (address < 16)
+                Serial.print("0");
+            Serial.println(address, HEX);
+        }
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("done\n");
+
+    delay(5000);
+}
+
+void printEvent(sensors_event_t* event) {
+    double x = -1000000, y = -1000000, z = -1000000; //dumb values, easy to spot problem
+    if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+        Serial.print("Accl:");
+        x = event->acceleration.x;
+        y = event->acceleration.y;
+        z = event->acceleration.z;
+    }
+    else if (event->type == SENSOR_TYPE_ORIENTATION) {
+        Serial.print("Orient:");
+        x = event->orientation.x;
+        y = event->orientation.y;
+        z = event->orientation.z;
+    }
+    else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+        Serial.print("Mag:");
+        x = event->magnetic.x;
+        y = event->magnetic.y;
+        z = event->magnetic.z;
+    }
+    else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+        Serial.print("Gyro:");
+        x = event->gyro.x;
+        y = event->gyro.y;
+        z = event->gyro.z;
+    }
+    else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+        Serial.print("Rot:");
+        x = event->gyro.x;
+        y = event->gyro.y;
+        z = event->gyro.z;
+    }
+    else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+        Serial.print("Linear:");
+        x = event->acceleration.x;
+        y = event->acceleration.y;
+        z = event->acceleration.z;
+    }
+    else if (event->type == SENSOR_TYPE_GRAVITY) {
+        Serial.print("Gravity:");
+        x = event->acceleration.x;
+        y = event->acceleration.y;
+        z = event->acceleration.z;
+    }
+    else {
+        Serial.print("Unk:");
+    }
+
+    Serial.print("\tx= ");
+    Serial.print(x);
+    Serial.print(" |\ty= ");
+    Serial.print(y);
+    Serial.print(" |\tz= ");
+    Serial.println(z);
 }
--- src/imu/imu.h
+++ src/imu/imu.h
@@ -1,3 +1,13 @@
+#include <Adafruit_Sensor.h>
+
+void displayData();
+
+void checkConnection();
+
+void printEvent(sensors_event_t* event);
+
+void displaySensorDetails();
+
 void setupIMU();
 
 void loopIMU();
