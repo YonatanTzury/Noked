eb9e75469bd37e0c2ef4b88caa05077b9884b153
Yonatan Tzury <yonatan@hud.io>
2026-06-08 20:52:25 +0300

    lora strong init

--- src/lora/lora.cpp
+++ src/lora/lora.cpp
@@ -3,10 +3,17 @@
 
 bool Lora::init(int nss, int rst, int dio0) {
   Lora::_lora.setPins(nss, rst, dio0);
-  int res = Lora::_lora.begin(433E6);
-  Lora::_lora.enableCrc();
-  Lora::_lora.setSyncWord(0x34);
-  return res;
+  for (int i = 0; i < 10; i++) {
+    if (Lora::_lora.begin(433E6)) {
+      Lora::_lora.enableCrc();
+      Lora::_lora.setSyncWord(0x34);
+      return true;
+    }
+
+    delay(1000);
+  }
+
+  return false;
 }
 
 int Lora::rssi() {
